71dab996330c3a0e809374d6f00c9d26dd7eb8dd
radovansurlak <radovansurlak@gmail.com>
2016-10-10 09:31:27 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title></title>
+
+  <meta charset="utf-8">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <link rel="stylesheet" href="css/main.css">
+
+</head>
+
+<body>
+
+</body>
+
+<script src="js/scripts.js"></script>
+</html>
