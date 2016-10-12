8fd21f5f3cde19efb21c58755964f255d97aa645
radovansurlak <radovansurlak@gmail.com>
2016-10-12 14:33:32 +0200

    pre-final commit

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-  <title></title>
+  <title>BRING IN | Doc Lounge Aarhus</title>
 
   <meta charset="utf-8">
   <meta name="description" content="">
@@ -11,7 +11,7 @@
   <link rel="stylesheet" href="css/main.css">
   <link rel="stylesheet" href="css/fonts.css">
 
-  <script src="http://code.jquery.com/jquery-3.1.1.slim.min.js"   integrity="sha256-/SIrNqv8h6QGKDuNoLGA4iret+kyesCkHGzVUUV0shc="   crossorigin="anonymous"></script>
+<script   src="https://code.jquery.com/jquery-3.1.1.min.js"   integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="   crossorigin="anonymous"></script>
 
 </head>
 
@@ -22,49 +22,61 @@
     <header>
       <nav id="menu">
         <ul>
-          <img id="logo" src="media/png_logo1.png">
-          <a href="#"><li>about</li></a>
-          <a href="#"><li>work with us</li></a>
-          <a href="#"><li>upcoming events</li></a>
-          <a href="#"><li>FAQ</li></a>
-          <a href="#"><li id="cta">contact us</li></a>
+          <li><img id="logo" src="media/png_logo1.png" alt="Bring In Doc Lounge logo"></li>
+
+          <div id="menu-wrap">
+            <a href="#intro"><img id="home" src="media/home.png" alt="home icon"></a>
+            <a href="#description"><li>about</li></a>
+            <a href="#benefits"><li>work with us</li></a>
+            <a href="#events"><li>upcoming events</li></a>
+            <a href="#faq"><li>FAQ</li></a>
+            <a href="#contact"><li id="cta">contact us</li></a>
+          </div>
+
         </ul>
       </nav>
     </header>
 
     <div id="bg-cont">
 
-      <video id="bg-vid" loop muted autoplay poster="" poster="">
+      <video id="bg-vid" loop muted controls autoplay poster="media/poster.png">
         <source src="media/bg.mp4" type="video/mp4">
       </video>
 
+      <div id="vid-overlay">
+        <h1 id="headline">Meet the DocLounge</h1>
+        <h2 id="subheadline">Chia craft beer assumenda lumbersexual. Pour-over occupy kogi sint labore celiac quinoa, slow-carb pitchfork et chicharrones. Freegan tote bag cardigan, dreamcatcher tumblr williamsburg gochujang authentic vape lyft affogato pug tofu mollit.</h2>
+      </div>
+
     </div>
 
-    <h1 id="headline">Meet the DocLounge</h1>
-    <h2 id="subheadline">Chia craft beer assumenda lumbersexual. Pour-over occupy kogi sint labore celiac quinoa, slow-carb pitchfork et chicharrones. Freegan tote bag cardigan, dreamcatcher tumblr williamsburg gochujang authentic vape lyft affogato pug tofu mollit.</h2>
 
 
   </section>
 
-  <section id="description">
+  <!-- DESCRIPTION SECTION -->
+
+  <section class="section" id="description">
 
     <h1 class="headline">Description<br>of the campaign</h1>
     <h2 class="subheadline">Chia craft beer assumenda lumbersexual. Pour-over occupy kogi sint labore celiac quinoa, slow-carb pitchfork et chicharrones. Freegan tote bag cardigan, dreamcatcher tumblr williamsburg gochujang authentic vape lyft affogato pug tofu mollit. <br><br> Do lumbersexual sriracha, artisan ea pok pok put a bird on it officia health goth irony tumblr elit you probably haven't heard of them tofu deep v. La croix readymade hella, distillery enamel pin est consequat whatever mixtape heirloom roof party mumblecore migas stumptown vice</h2>
 
-  <section>
+  </section>
 
-  <section id="benefits">
+  <!-- BENEFITS SECTION -->
+
+  <section class="section" id="benefits">
 
     <div class="screen">
 
       <div class="benefit-bar">
-        <img src="media/megaphone.png">
+        <img src="media/magnet.png">
         <h2 class="subheadline">Reach broader audience</h2>
         <p class="description">Chia craft beer assumenda lumbersexual. Pour-over occupy kogi sint labore celiac quinoa, slow-carb pitchfork et chicharrones. Freegan tote bag cardigan, dreamcatcher tumblr williamsburg gochujang authentic vape lyft affogato pug tofu mollit. Do lumbersexual sriracha</p>
       </div>
 
       <div class="benefit-bar">
-        <img src="media/magnet.png">
+        <img src="media/megaphone.png">
         <h2 class="subheadline">Promote your business</h2>
         <p class="description">Chia craft beer assumenda lumbersexual. Pour-over occupy kogi sint labore celiac quinoa, slow-carb pitchfork et chicharrones. Freegan tote bag cardigan, dreamcatcher tumblr williamsburg gochujang authentic vape lyft affogato pug tofu mollit. Do lumbersexual sriracha</p>
       </div>
@@ -77,19 +89,198 @@
 
     </div>
 
-  <section>
+  </section>
 
-  <section id="events">
+<!-- EVENTS SECTION -->
+
+  <section class="section" id="events">
 
     <h1 class="headline">Events</h1>
-    <div class="event-bar">
-      <img>
-      <h2 class="subheadline">Film name</h2>
+
+    <div class="screen">
+
+      <div class="event-bar">
+        <div class="img-wrap">
+          <img src="media/events/1.png">
+        </div>
+        <h2 class="subheadline">Bugs</h2>
+        <hr></hr>
+        <p class="film-description">Directors: Andreas Johnsen<br>76 min, Denmark<br><hr><br>
+        Why should we eat insects? Filmmaker Andreas Johnsen’s partly tasty and partly a tiny tiny bit repulsive documentary Bugs sets out to discover if eating insects can save our Earth. The result is equal parts culinary documentary and political conversation starter.</p>
+        <button type="button">Read more</button>
+      </div>
+
+      <div class="event-bar">
+        <div class="img-wrap">
+          <img src="media/events/2.png">
+        </div>
+        <h2 class="subheadline">At Home In The World</h2>
+        <hr></hr>
+        <p class="film-description">Directors: Andreas Koefoed<br>58 min, Denmark<br><hr><br>
+          At Home In The World is an intimate depiction of the everyday lives of five refugee children on a Danish Red Cross asylum school.
+          The children; Magomed, Sehmuz, Heda, Amel and Ali have different nationalities and backgrounds, but they have all fled their homes with their families, arriving in Denmark with the hope of starting over.</p>
+        <button type="button">Read more</button>
+      </div>
+
+      <div class="event-bar">
+        <div class="img-wrap">
+          <img src="media/events/3.png">
+        </div>
+        <h2 class="subheadline">Misfits</h2>
+        <hr></hr>
+        <p class="film-description">Directors: Jannik Splidsboel<br>74 min, Denmark<br><hr><br>
+          Misfits a coming of age-doc about three American teenagers from conservative Tulsa struggling with isolation and instability in a heartfelt story that portrays family bond, poverty, survival, love and the consequences of coming out as a young LGBT in the Bible Belt.</p>
+        <button type="button">Read more</button>
+      </div>
 
     </div>
-    <h2 class="subheadline">Chia craft beer assumenda lumbersexual. Pour-over occupy kogi sint labore celiac quinoa, slow-carb pitchfork et chicharrones. Freegan tote bag cardigan, dreamcatcher tumblr williamsburg gochujang authentic vape lyft affogato pug tofu mollit. <br><br> Do lumbersexual sriracha, artisan ea pok pok put a bird on it officia health goth irony tumblr elit you probably haven't heard of them tofu deep v. La croix readymade hella, distillery enamel pin est consequat whatever mixtape heirloom roof party mumblecore migas stumptown vice</h2>
 
-  <section>
+  </section>
+
+
+  <!-- FAQ SECTION -->
+
+  <section class="section" id="faq">
+    <h1 class="headline">FAQ<h1>
+    <img src="media/chair.png">
+
+    <div id="faq-screen">
+
+      <ul class="question">
+        <li>
+          <h2 class="subheadline">Should we book the cleaning staff to clean up after you? </h2>
+        </li>
+          <p>Don’t worry, we take the full responsibility for cleaning up after ourselves</p>
+        <li>
+          <h2 class="subheadline">How many people will you bring to our venue?</h2>
+        </li>
+          <p>The average amount is about 40-50, but it can vary according to theme of the movie.</p>
+        <li>
+          <h2 class="subheadline">How long will it take?</h2>
+        </li>
+          <p>The event takes about 3 hours and we will need a couple hours before and after to prepare and to clean it all up.</p>
+      </ul>
+
+    </div>
+  </section>
+
+  <!-- GALLERY SECTION -->
+
+  <section id="gallery">
+
+    <?php
+
+       error_reporting(0);
+
+      $images = scandir("media/gallery");
+
+      foreach ($images as $image ) {
+
+        $extensions = array(".jpeg","jpg",".png");
+
+        $image_info = getimagesize("media/gallery/$image");
+        $filename = pathinfo("media/$image", PATHINFO_FILENAME);
+
+        if($image_info){
+          echo "<div class=\"img-wrap\"><a href=\"media/gallery/$image\"><img id=\"gallery-image\" src=\"media/gallery/$image\" alt=\"gallery image\" /></a></div>";
+        }
+      };
+    ?>
+
+  </section>
+
+  <!-- GALLERY SECTION -->
+
+  <section class="section" id="testimonials">
+
+    <div class="screen">
+
+      <div class="testimonial-bar">
+        <h2 class="headline">Marie Vilhelmsen,<br>Youth Church Aarhus</h2>
+        <p>“It is always nice to expand your audience and by hosting Doc Lounce events, we can see that more people get to know about our church”</p>
+        <img id="portrait-img" src="media/testimonials/review1.png">
+      </div>
+
+      <div class="testimonial-bar">
+        <h2 class="headline">Kasper Mogensen,<br>Godsbanen</h2>
+        <p>“From a business perspective we have noticed a small increase in our profit and amount of visitors since we hosted one of the Doc Lounge events. Personally I even like to participate myself”</p>
+        <img id="portrait-img" src="media/testimonials/review2.png">
+      </div>
+
+      <div class="testimonial-bar">
+        <h2 class="headline">Stine Madsen,<br>Filmbyen</h2>
+        <p>“We have hosted Doc Lounge events a couple of times now. I think it’s a great initiative and it is amazing how they can create such a nice atmosphere around the audience”</p>
+        <img id="portrait-img" src="media/testimonials/review3.png">
+      </div>
+
+  </section>
+
+  <!-- SOCIAL PROOF SECTION -->
+
+  <section class="section" id="social-proof">
+
+    <div class="screen">
+      <?php
+      error_reporting(0);
+
+     $images = scandir("media/logos");
+
+     foreach ($images as $image ) {
+
+       $extensions = array(".jpeg","jpg",".png");
+
+       $image_info = getimagesize("media/logos/$image");
+       $filename = pathinfo("media/$image", PATHINFO_FILENAME);
+
+       if($image_info){
+         echo "<div class=\"logo-wrap\"><img id=\"partner-logo\" src=\"media/logos/$image\" alt=\"partner logo\"/></div>";
+       }
+     };
+
+      ?>
+    </div>
+
+  </section>
+
+  <!-- CONTACT SECTION -->
+
+  <section class="section" id="contact">
+
+    <h2 class="headline">Contact us</h2>
+
+    <form action="/my-handling-form-page" method="post">
+
+      <div id="form-wrap">
+          <input type="text" id="name" name="user_name" placeholder="Name*"/>
+          <input type="email" id="mail" name="user_mail" placeholder="E-mail*" />
+      </div>
+
+          <textarea id="msg" name="user_message" placeholder="Message*" ></textarea>
+
+
+        <button type="submit">Submit</button>
+
+    </form>
+
+
+
+    <div id="social-icons">
+      <div class="social">
+        <a href="https://www.facebook.com/doclounge.aarhus"><img src="media/social/fb.png"></a>
+      </div>
+      <div class="social">
+        <a href="https://twitter.com/docloungeaarhus"><img src="media/social/twit.png"></a>
+      </div>
+    </div>
+
+  </section>
+
+
+
+
+
+
+
 
 
 </body>
--- js/jquery.js
+++ js/jquery.js
@@ -1,10 +1,32 @@
+$("#menu").on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 750);
+});
+
+var $vid = $('video','#bg-cont');
+var $msg = $('#vid-overlay');
+$msg.css({top:($vid.height()/2)-$msg.height()/2});
+
+$( window ).resize(function() {
+  var $vid = $('video','#bg-cont');
+  var $msg = $('#vid-overlay');
+  $msg.css({top:($vid.height()/2)-$msg.height()/2});
+});
+
+
 $(window).scroll(function() {
     if ($(window).scrollTop() > 100) {
         $("#logo").css("height","11vw");
-        $("#menu").delay("300ms").css("background-color","rgba(0,0,0,1)");
+        $("#menu").delay("300ms").css("height","5vw");
+        // $("#menu").delay("300ms").css("background-image","url(media/menu-bg.jpg)");
+
+        // $("#menu").delay("300ms").css("background-color","rgba(0,0,0,1)");
     }
     else {
-        $("#menu").css("background-color","unset");
+        $("#menu").css("height","0vw");
         $("#logo").delay("300ms").css("height","17vw");
 
     }
